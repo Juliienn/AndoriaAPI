7d7a477ffaef8630d26d5f6570e0b2c398d46203
BuildTools <unconfigured@null.spigotmc.org>
2023-07-22 02:06:39 +0200

    Updating some bugs

--- src/main/java/fr/andoriaapi/database/sql/DatabaseManager.java
+++ src/main/java/fr/andoriaapi/database/sql/DatabaseManager.java
@@ -2,9 +2,9 @@ package fr.andoriaapi.database.sql;
 
 public enum DatabaseManager {
 
-    PLAYERS(new DatabaseConnector("51.254.39.58", "root", "Champagne2003@", "andoriaplayers", 3306)),
-    FACTIONS(new DatabaseConnector("51.254.39.58", "root", "Champagne2003@", "andoriafactions", 3306)),
-    SANCTIONS(new DatabaseConnector("51.254.39.58", "root", "Champagne2003@", "andoriasanctions", 3306));
+    PLAYERS(new DatabaseConnector("localhost", "root", "Champagne2003@", "andoriaplayers", 3306)),
+    FACTIONS(new DatabaseConnector("localhost", "root", "Champagne2003@", "andoriafactions", 3306)),
+    SANCTIONS(new DatabaseConnector("localhost", "root", "Champagne2003@", "andoriasanctions", 3306));
 
     private final DatabaseConnection databaseConnection;
 
